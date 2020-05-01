4af5fff37cbb6c76f00581d62407d22e7c96c5ac
Nevena <nvndjukin97@gmail.com>
2020-05-02 00:03:35 +0200

    Added IssuerData to model

---
+++ xml-service/src/main/java/com/model/IssuerData.java
@@ -0,0 +1,17 @@
+package com.model;
+
+import lombok.*;
+import org.bouncycastle.asn1.x500.X500Name;
+
+import java.security.PrivateKey;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class IssuerData {
+    private PrivateKey privateKey;
+    private X500Name x500name;
+
+}
