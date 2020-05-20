c944dde7aaefd7fa273236ab1d6dd5a2d22aeaf5
Nevena <nvndjukin97@gmail.com>
2020-05-20 20:42:59 +0200

    Added repository

--- xml-service-messages/src/main/java/com/example/repository/PorukaRepository.java
+++ xml-service-messages/src/main/java/com/example/repository/PorukaRepository.java
@@ -1,4 +1,7 @@
 package com.example.repository;
 
-public class PorukaRepository {
+import com.example.model.Poruka;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PorukaRepository extends JpaRepository<Long, Poruka> {
 }
