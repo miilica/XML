6195455a39412dbdf2bb633475b62e16ee6e63aa
miilica <milicaradovanovic64@gmail.com>
2020-05-21 20:42:04 +0200

    Create UserDTO.java

---
+++ xml-service-user/src/main/java/com/example/dto/UserDTO.java
@@ -0,0 +1,48 @@
+package com.example.dto;
+
+import com.example.model.Authority;
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+
+import java.security.Timestamp;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    private Long id;
+    private String name;
+    private String surname;
+    private String username;
+    private String ucidn;
+    private String address;
+    private String city;
+    private String country;
+    private String email;
+    private String phone;
+    private String password;
+    private  String role;
+    private boolean isAdmin;
+    private Timestamp lastPasswordResetDate;
+    private List<Authority> authorities;
+
+    public UserDTO(Long id, String name, String surname, String username, String ucidn, String address, String city, String country, String email, String phone, String password, String role, boolean isAdmin, Timestamp lastPasswordResetDate, List<Authority> authorities) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.ucidn = ucidn;
+        this.address = address;
+        this.city = city;
+        this.country = country;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.role = role;
+        this.isAdmin = isAdmin;
+        this.lastPasswordResetDate = lastPasswordResetDate;
+        this.authorities = authorities;
+    }
+}
