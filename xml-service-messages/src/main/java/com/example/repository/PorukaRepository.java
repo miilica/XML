ef6bab2233e700ef8068c126cd1a7ea0e0093396
Nevena <nvndjukin97@gmail.com>
2020-05-24 23:56:59 +0200

    Configuring eureka clients

--- xml-service-messages/src/main/java/com/example/repository/PorukaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.model.Poruka;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PorukaRepository extends JpaRepository<Long, Poruka> {
-}
