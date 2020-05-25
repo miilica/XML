7a1c57eeb1906e64db42e7af7af03e1f68ad2bbe
Nevena <nvndjukin97@gmail.com>
2020-05-25 22:14:47 +0200

    Added ads microservice

---
+++ xml-service-oglas/src/main/java/com/example/xmlserviceoglas/XmlServiceOglasApplication.java
@@ -0,0 +1,13 @@
+package com.example.xmlserviceoglas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlServiceOglasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlServiceOglasApplication.class, args);
+	}
+
+}
---
+++ xml-service-oglas/src/test/java/com/example/xmlserviceoglas/XmlServiceOglasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xmlserviceoglas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlServiceOglasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
