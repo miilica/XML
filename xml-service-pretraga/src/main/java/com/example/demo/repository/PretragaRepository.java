f3276cf55d308b2d651b2c30014f5337f67affc4
Nevena <nvndjukin97@gmail.com>
2020-05-29 00:42:39 +0200

    Added search database schema

---
+++ xml-service-pretraga/src/main/java/com/example/demo/repository/PretragaRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Pretraga;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PretragaRepository extends JpaRepository<Pretraga, Long> {
+}
