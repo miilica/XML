2bf55f4f7f54afae0702701f349cf0cba65cbfe0
Katarina Prodanovic <57413828+Katarina1910@users.noreply.github.com>
2020-05-28 19:36:42 +0200

    Male izmjene u message i search service-u

--- xml-service-pretraga/src/main/java/com/example/demo/model/Pretraga.java
+++ xml-service-pretraga/src/main/java/com/example/demo/model/Pretraga.java
@@ -29,13 +29,16 @@ public class Pretraga {
     private String modelVozila;
 
     @Column
+    private String markaVozila;
+
+    @Column
     private String klasaVozila;
 
     @Column
     private double kilometraza;
 
     @Column
-    private int cijena;
+    private double cijena;
 
     @Column
     private double ocjena;
@@ -49,91 +52,4 @@ public class Pretraga {
     @Column
     private String imeKompanije;
 
-    public Long getId() {
-        return id;
-    }
-
-    public Date getOd() {
-        return od;
-    }
-
-    public Date getDoo() {
-        return doo;
-    }
-
-    public String getModelVozila() {
-        return modelVozila;
-    }
-
-    public String getKlasaVozila() {
-        return klasaVozila;
-    }
-
-    public double getKilometraza() {
-        return kilometraza;
-    }
-
-    public int getCijena() {
-        return cijena;
-    }
-
-    public double getOcjena() {
-        return ocjena;
-    }
-
-    public VrstaGoriva getGorivo() {
-        return gorivo;
-    }
-
-    public TipMjenjanca getMjenjac() {
-        return mjenjac;
-    }
-
-    public String getImeKompanije() {
-        return imeKompanije;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setOd(Date od) {
-        this.od = od;
-    }
-
-    public void setDoo(Date doo) {
-        this.doo = doo;
-    }
-
-    public void setModelVozila(String modelVozila) {
-        this.modelVozila = modelVozila;
-    }
-
-    public void setKlasaVozila(String klasaVozila) {
-        this.klasaVozila = klasaVozila;
-    }
-
-    public void setKilometraza(double kilometraza) {
-        this.kilometraza = kilometraza;
-    }
-
-    public void setCijena(int cijena) {
-        this.cijena = cijena;
-    }
-
-    public void setOcjena(double ocjena) {
-        this.ocjena = ocjena;
-    }
-
-    public void setGorivo(VrstaGoriva gorivo) {
-        this.gorivo = gorivo;
-    }
-
-    public void setMjenjac(TipMjenjanca mjenjac) {
-        this.mjenjac = mjenjac;
-    }
-
-    public void setImeKompanije(String imeKompanije) {
-        this.imeKompanije = imeKompanije;
-    }
 }
