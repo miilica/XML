20eb44b44436741ce6b50ac87a064708bee0765b
Katarina Prodanovic <57413828+Katarina1910@users.noreply.github.com>
2020-05-28 19:38:12 +0200

    Izmjene u vozilo service-u

---
+++ xml-service-vozilo/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,59 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static javax.persistence.InheritanceType.JOINED;
+
+@Entity
+@Inheritance(strategy=JOINED)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String surname;
+
+    @Column
+    private String username;
+
+    @Column
+    private String ucidn;
+
+    @Column
+    private String address;
+
+    @Column
+    private String city;
+
+    @Column
+    private String country;
+
+    @Column
+    private String email;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String password;
+
+    @Column
+    private  String role;
+
+    @Column
+    private boolean isAdmin;
+}
