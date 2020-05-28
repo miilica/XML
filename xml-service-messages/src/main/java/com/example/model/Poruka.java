a0fd03bb588ab97dd55baf1b8ac9335924223f20
Nevena <nvndjukin97@gmail.com>
2020-05-29 00:19:43 +0200

    Added message database schema

--- xml-service-messages/src/main/java/com/example/model/Poruka.java
+++ xml-service-messages/src/main/java/com/example/model/Poruka.java
@@ -1,17 +1,34 @@
 package com.example.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Poruka {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column
     private String sadrzaj;
 
+    @Column
     private Date datum;
 
     //private User poslijalac;
+    @Column
     private String senderId;
 
     //private User primalac;
+    @Column
     private String reciverId;
 }
