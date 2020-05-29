49a3098d50e2af57ea3dd4ad20edb76365aa9e7f
Nevena <nvndjukin97@gmail.com>
2020-05-29 22:20:37 +0200

    Configured ads db schema

---
+++ xml-service-oglas/src/main/java/com/example/xmlserviceoglas/repository/OglasRepository.java
@@ -0,0 +1,7 @@
+package com.example.xmlserviceoglas.repository;
+
+import com.example.xmlserviceoglas.model.Oglas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OglasRepository extends JpaRepository<Oglas, Long> {
+}
