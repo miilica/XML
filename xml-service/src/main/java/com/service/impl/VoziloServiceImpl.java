17b40e25224ebe44cb75d2fd2efb5349c166c96b
Dusan <dusan.dsavic.97@gmail.com>
2020-06-18 22:09:12 +0200

    agent dodaje kilometrazu backend

--- xml-service/src/main/java/com/service/impl/VoziloServiceImpl.java
+++ xml-service/src/main/java/com/service/impl/VoziloServiceImpl.java
@@ -1,16 +1,20 @@
 package com.service.impl;
 
 import com.dto.VoziloDTO;
-import com.model.KorpaVozila;
-import com.model.Oglas;
-import com.model.Vozilo;
-import com.repository.KorpaVozilaRepository;
+import com.model.*;
+import com.repository.TerminIznajmljivanjaRepository;
+import com.repository.UserRepository;
 import com.repository.VoziloRepository;
 import com.service.VoziloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -18,6 +22,12 @@ public class VoziloServiceImpl implements VoziloService {
 
     @Autowired
     private VoziloRepository voziloRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TerminIznajmljivanjaRepository terminIznajmljivanjaRepository;
 
     @Override
     public List<Vozilo> findAll() {
@@ -46,6 +56,45 @@ public class VoziloServiceImpl implements VoziloService {
         m.setColiisionDamageWavier(vDTO.isColiisionDamageWavier());
         m = this.voziloRepository.save(m);
         return m;
+    }
+
+    @Override
+    public List<TerminIznajmljivanja> findAllTermineIznajmljivanja() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Object currentUser = auth.getPrincipal();
+
+        String username = "";
+        if (currentUser instanceof UserDetails) {
+            username = ((UserDetails)currentUser).getUsername();
+        } else {
+            username = currentUser.toString();
+        }
+
+        User u = userRepository.findByUsername(username);
+
+        List<TerminIznajmljivanja> all = terminIznajmljivanjaRepository.findAll();
+        List<TerminIznajmljivanja> result = new ArrayList<>();
+        Date currentUtilDate = new Date();
+
+        for(TerminIznajmljivanja ti : all) {
+            if(ti.getVozilo().getAgent().getId() == u.getId()) {
+                if(ti.getDoKad().before(currentUtilDate)) {
+                    result.add(ti);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public Vozilo addKmToVehicle(VoziloDTO voziloDTO, Double predjeniKM){
+        Vozilo vozilo = voziloRepository.findById(voziloDTO.getId()).orElseGet(null);
+        Double km = vozilo.getKilometraza() + predjeniKM;
+        vozilo.setKilometraza(km);
+        vozilo = voziloRepository.save(vozilo);
+
+        return vozilo;
     }
 
 }
