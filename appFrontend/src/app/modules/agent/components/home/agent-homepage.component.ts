1ddda50421dd531c4bb18ae4c4eeb99b9ce69077
Dusan <dusan.dsavic.97@gmail.com>
2020-06-18 22:08:07 +0200

    agent dodaje kilometrazu frontend

--- appFrontend/src/app/modules/agent/components/home/agent-homepage.component.ts
+++ appFrontend/src/app/modules/agent/components/home/agent-homepage.component.ts
@@ -1,7 +1,7 @@
 import { OnInit, Component } from '@angular/core';
 import { ZAHTJEVI_ZA_VOZILA_URL } from 'src/app/config/api-paths';
 import { Router } from '@angular/router';
-import { ZAHTJEVI_ZA_VOZILA_AGENT_PATH } from 'src/app/config/router-paths';
+import { ZAHTJEVI_ZA_VOZILA_AGENT_PATH, TERMINI_IZNAJMLJENIH_ZA_VOZILA_AGENT_PATH } from 'src/app/config/router-paths';
 
 @Component({
     selector: 'app-agent-homepage',
@@ -19,6 +19,10 @@ import { ZAHTJEVI_ZA_VOZILA_AGENT_PATH } from 'src/app/config/router-paths';
     onClickZahtjevi(): void {
         this.router.navigate([ZAHTJEVI_ZA_VOZILA_AGENT_PATH]);
     }
+
+    onClickTermini(): void {
+      this.router.navigate([TERMINI_IZNAJMLJENIH_ZA_VOZILA_AGENT_PATH]);
+  }
 
   }
   
--- appFrontend/src/app/services/ads.service.ts
+++ appFrontend/src/app/services/ads.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { USER_ALL_ADS_URL, USER_ALL_VEHICLES_URL, VEHICLE_TO_CART_URL, ALL_VEHICLEs_IN_CART_URL, RENT_A_CAR_REQUEST_URL, ZAHTJEVI_ZA_VOZILA_URL, VEHICLE_STATUS_TO_RESERVED_URL, VEHICLE_STATUS_TO_CANCELED_URL, FIND_VEHICLE_BY_ID_URL, PAY_FOR_RENT_A_CAR_REQUEST_URL, RENT_A_CAR_BUNDLE_REQUEST_URL, VEHICLE_STATUS_TO_RESERVED_BUNDLE_URL, USER_RATE_COMMENT_URL, USER_RATE_VEHICLE_URL, USER_COMMENT_VEHICLE_URL } from '../config/api-paths';
+import { USER_ALL_ADS_URL, USER_ALL_VEHICLES_URL, VEHICLE_TO_CART_URL, ALL_VEHICLEs_IN_CART_URL, RENT_A_CAR_REQUEST_URL, ZAHTJEVI_ZA_VOZILA_URL, VEHICLE_STATUS_TO_RESERVED_URL, VEHICLE_STATUS_TO_CANCELED_URL, FIND_VEHICLE_BY_ID_URL, PAY_FOR_RENT_A_CAR_REQUEST_URL, RENT_A_CAR_BUNDLE_REQUEST_URL, VEHICLE_STATUS_TO_RESERVED_BUNDLE_URL, USER_RATE_COMMENT_URL, USER_RATE_VEHICLE_URL, USER_COMMENT_VEHICLE_URL, AGENT_ALL_TERMINI_URL, AGENT_ADD_KM_VEHICLE_URL } from '../config/api-paths';
 import VehicleDTO from '../components/models/vehicle-dto.model';
 import ZahtjevDTO from '../components/models/zahtjev-dto.model';
 import { Vozilo } from '../components/dodajVozilo/vozilo';
@@ -76,12 +76,20 @@ export class AdsService {
         return this.http.post(PAY_FOR_RENT_A_CAR_REQUEST_URL, zahtjev);
     }
 
-    rateVehicle(z: Vozilo, rate: any) {
-        return this.http.put<Vozilo>(`${USER_RATE_VEHICLE_URL}/${rate}`, z);
+    rateVehicle(v: Vozilo, rate: any) {
+        return this.http.put<Vozilo>(`${USER_RATE_VEHICLE_URL}/${rate}`, v);
     }
 
-    commentVehicle(z: Vozilo, comment: any) {
-        return this.http.put<Vozilo>(`${USER_COMMENT_VEHICLE_URL}/${comment}`, z);
+    commentVehicle(v: Vozilo, comment: any) {
+        return this.http.put<Vozilo>(`${USER_COMMENT_VEHICLE_URL}/${comment}`, v);
+    }
+
+    getAllTermini(): Observable<any> {
+        return this.http.get(AGENT_ALL_TERMINI_URL);
+    }
+
+    addKmToVehicle(v: Vozilo, brojKM: number) {
+        return this.http.put<Vozilo>(`${AGENT_ADD_KM_VEHICLE_URL}/${brojKM}`, v);
     }
 
 }
