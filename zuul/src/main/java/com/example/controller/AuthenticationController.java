1c13e6e87120db3a960d0a45f54ade9ae2f1a78b
Dusan <dusan.dsavic.97@gmail.com>
2020-07-10 17:23:14 +0200

    added security to zuul

---
+++ zuul/src/main/java/com/example/controller/AuthenticationController.java
@@ -0,0 +1,69 @@
+package com.example.controller;
+
+import com.example.model.User;
+import com.example.model.UserTokenState;
+import com.example.security.TokenUtils;
+import com.example.service.UserService;
+import com.example.service.impl.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+//Kontroler zaduzen za autentifikaciju korisnika
+@RestController
+@RequestMapping(value = "/auth", produces = MediaType.APPLICATION_JSON_VALUE)
+public class AuthenticationController {
+
+	@Autowired
+	private TokenUtils tokenUtils;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private CustomUserDetailsService userDetailsService;
+
+	@Autowired
+	private UserService userService;
+
+	// U slucaju isteka vazenja JWT tokena, endpoint koji se poziva da se token osvezi
+	@PostMapping(value = "/refresh")
+	public ResponseEntity<UserTokenState> refreshAuthenticationToken(HttpServletRequest request) {
+
+		String token = tokenUtils.getToken(request);
+		String username = this.tokenUtils.getUsernameFromToken(token);
+		User user = (User) this.userDetailsService.loadUserByUsername(username);
+
+		if (this.tokenUtils.canTokenBeRefreshed(token, user.getLastPasswordResetDate())) {
+			String refreshedToken = tokenUtils.refreshToken(token);
+			int expiresIn = tokenUtils.getExpiredIn();
+
+			return ResponseEntity.ok(new UserTokenState(refreshedToken, expiresIn));
+		} else {
+			UserTokenState userTokenState = new UserTokenState();
+			return ResponseEntity.badRequest().body(userTokenState);
+		}
+	}
+
+	@RequestMapping(value = "/change-password", method = RequestMethod.POST)
+	@PreAuthorize("hasRole('USER')")
+	public ResponseEntity<?> changePassword(@RequestBody PasswordChanger passwordChanger) {
+		userDetailsService.changePassword(passwordChanger.oldPassword, passwordChanger.newPassword);
+
+		Map<String, String> result = new HashMap<>();
+		result.put("result", "success");
+		return ResponseEntity.accepted().body(result);
+	}
+
+	static class PasswordChanger {
+		public String oldPassword;
+		public String newPassword;
+	}
+}
